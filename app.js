5438fd51bdac424210670fb19ab7bc61129bc838
Marcelinaidoko <mirabeluregu@gmail.com>
2024-12-04 03:20:20 +0100

    cloudinary and multer

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 2024;
+const app = express();
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+      message: "Server is uP and Running 🚀🚀🚀",
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log("Server is Up on " + PORT);
+  });
